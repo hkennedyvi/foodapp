dc01f90036fed73b9c2ecea9c7083720bdb712fe
Howard Kennedy <hkennedyvi@gmail.com>
2020-09-14 16:17:43 -0700

    test page/components created

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NavBar() {
+   
+
+    return (
+        <div>
+            NAVBAR TEST
+        </div>
+    );
+}
+
+export default NavBar;
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+function  Home () {
+
+    
+
+    return (
+        <div>
+            <Navbar />
+            PAGE TEST
+        </div>
+    )
+}
+
+export default Home;
