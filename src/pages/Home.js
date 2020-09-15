4af9faf36b5cfa954b5fa5f4ba883dfc6344b08d
Howard Kennedy <hkennedyvi@gmail.com>
2020-09-14 17:02:45 -0700

    icon attribution

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,6 +7,10 @@ function  Home () {
         <div>
             <Navbar />
             PAGE TEST
+            Icons made by 
+            <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>
+            from 
+            <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
         </div>
     )
 }
