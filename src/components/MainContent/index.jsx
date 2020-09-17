ced88a6668c1bd50ac87dd93f1ccbf1a03bf22e5
Howard Kennedy <hkennedyvi@gmail.com>
2020-09-17 15:38:19 -0700

    main content trim

--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -9,7 +9,10 @@ function MainContent() {
             </div>
             <div className="main-blurb">
                 <p className="main-blurb-text">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Elementum sagittis vitae et leo duis ut diam. Auctor eu augue ut lectus. Sit amet aliquam id diam maecenas ultricies. Aliquam id diam maecenas ultricies mi eget mauris pharetra et. Pellentesque elit eget gravida cum. Sit amet consectetur adipiscing elit. Duis ultricies lacus sed turpis tincidunt id aliquet. Sed elementum tempus egestas sed sed risus pretium quam. Pulvinar elementum integer enim neque volutpat ac tincidunt vitae semper. Massa sapien faucibus et molestie. Elit ut aliquam purus sit.
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Sit amet aliquam id diam maecenas ultricies. Aliquam id diam maecenas ultricies mi eget mauris pharetra et. 
+                    Pellentesque elit eget gravida cum. Sit amet consectetur adipiscing elit. Duis ultricies lacus sed turpis tincidunt. 
+                    Sed elementum tempus egestas sed sed risus pretium quam. Pulvinar elementum integer enim neque volutpat ac tincidunt. 
                 </p>
             </div>
         </main>
