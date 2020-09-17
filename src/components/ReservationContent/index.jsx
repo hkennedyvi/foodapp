49d9c3843fa0725683b0e7425eb2274c667e6de0
Howard Kennedy <hkennedyvi@gmail.com>
2020-09-17 16:30:43 -0700

    initial reservation form styling

--- src/components/ReservationContent/index.jsx
+++ src/components/ReservationContent/index.jsx
@@ -7,6 +7,46 @@ function ReservationContent() {
             <div className="reservation-header">
                 <h1>Make A Reservation</h1>
             </div>
+            <form className="reservation-form">
+                <label className="reservation-input">
+                    First Name:
+                    <input type="text" name="firstName" />
+                </label>
+                <label className="reservation-input">
+                    Last Name:
+                    <input type="text" name="lastName" />
+                </label>
+                <div>
+                <label className="reservation-input">
+                    Guests:
+                    <select>
+                        <option value="1">1</option>
+                        <option selected value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                    </select>
+                </label>
+                <label className="reservation-input">
+                    Time:
+                    <select>
+                        <option value="6:00">6:00</option>
+                        <option value="6:30">6:30</option>
+                        <option selected value="7:00">7:00</option>
+                        <option value="7:30">7:30</option>
+                    </select>
+                </label>
+                <label className="reservation-input">
+                    Date:
+                    <select>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option selected value="3">3</option>
+                        <option value="4">4</option>
+                    </select>
+                </label>
+                </div>
+                <input type="submit" value="Submit" />
+            </form>
         </main>
     )
 }
