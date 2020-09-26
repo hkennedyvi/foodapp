99da68eacd192eb556453b34e6f0b1434847079d
Howard Kennedy <hkennedyvi@gmail.com>
2020-09-26 12:33:53 -0700

    main page font and layout edits

--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -4,16 +4,17 @@ import './maincontent.scss';
 function MainContent() {
     return (
         <main className="main-home">
-            <div className="main-header">
-                <h1>good restauraunt.</h1>
+            <div>
+                <h1 className="main-header">good restauraunt.</h1>
             </div>
             <div className="main-blurb">
                 <p className="main-blurb-text">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
                     Sit amet aliquam id diam maecenas ultricies. Aliquam id diam maecenas ultricies mi eget mauris pharetra et. 
                     Pellentesque elit eget gravida cum. Sit amet consectetur adipiscing elit. Duis ultricies lacus sed turpis tincidunt. 
-                    Sed elementum tempus egestas sed sed risus pretium quam. Pulvinar elementum integer enim neque volutpat ac tincidunt. 
+                    {/* Sed elementum tempus egestas sed sed risus pretium quam. Pulvinar elementum integer enim neque volutpat ac tincidunt.  */}
                 </p>
+                <button className="button">ENTER</button>
             </div>
         </main>
     )
