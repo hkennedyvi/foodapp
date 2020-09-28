b0618366e839a9feab19b41020916b4bf71d9e61
Howard Kennedy <hkennedyvi@gmail.com>
2020-09-28 13:25:10 -0700

    backend/frontend run together

--- server.js
+++ server.js
@@ -1,10 +1,34 @@
-const http = require('http');
-const app = require('./app');
+const express = require('express');
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3002;
+const app = express();
 
-const server = http.createServer(app);
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-server.listen(PORT, function () {
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+  }
+
+const menuRoutes = require('./api/routes/menus');
+
+app.use('/menus', menuRoutes);
+
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "./client/build/index.html"));
+//   });
+  
+app.listen(PORT, function() {
     console.log(`🌎 ==> API server now on port ${PORT}!`);
-});
+  });
+
+// const http = require('http');
+// const app = require('./app');
+
+// const PORT = process.env.PORT || 8000;
+
+// const server = http.createServer(app);
+
+// server.listen(PORT, function () {
+//     console.log(`🌎 ==> API server now on port ${PORT}!`);
+// });
