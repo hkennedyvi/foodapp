72e97c2d0c156c7b4508316c12344aaf6e6efa30
Howard Kennedy <hkennedyvi@gmail.com>
2020-09-30 23:55:30 -0700

    new api entries

--- api/routes/menus.js
+++ api/routes/menus.js
@@ -1,27 +1,37 @@
+const { urlencoded } = require('express');
 const express = require('express');
 const router = express.Router();
 //Need to properly import image for menu items from assets folder
-// const burgerImage = url('../../assets/042-burger.png');
+// const burgerImage = require('../../assets/burger');
+
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
         breakfastMenu : {
             title: "breakfast.",
-            items: [{
+            items: [
+                {
                 title: "pancakes",
                 ingredients: "syrup, butter, hazelnuts",
                 price: 9,
-                // image: PancakeImage
+                image: ""
             },
             {
-                title: "sandwich",
-                ingredients: "turkey, bacon, avocado",
+                title: "waffles",
+                ingredients: "whipped cream, strawberries, bananas",
                 price: 8
-            }]
+            }, 
+            {
+                title: "donut",
+                ingredients: "glaze, chocolate, sprinkles",
+                price: 5
+            }
+        ]
         },
         lunchMenu : {
             title: "lunch.",
-            items: [{
+            items: [
+                {
                 title: "burger",
                 ingredients: "lettuce, tomato, onion",
                 price: 9,
@@ -31,21 +41,34 @@ router.get('/', (req, res, next) => {
                 title: "sandwich",
                 ingredients: "turkey, bacon, avocado",
                 price: 8
-            }]
+            },
+            {
+                title: "hot dog",
+                ingredients: "mustard, ketchup, relish",
+                price: 7
+            }
+        ]
         },
         dinnerMenu : {
             title: "dinner.",
-            items: [{
+            items: [
+                {
                 title: "pizza",
                 ingredients: "pepperoni, mushrooms, peppers",
                 price: 9,
                 // image: PizzaImage
             },
             {
-                title: "sandwich",
-                ingredients: "turkey, bacon, avocado",
+                title: "sushi",
+                ingredients: "salmon, rice, ginger",
                 price: 8
-            }]
+            },
+            {
+                title: "tacos",
+                ingredients: "beef, cheese, salsa",
+                price: 6
+            }
+        ]
         }
     })
 })
