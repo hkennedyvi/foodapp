d40d27f464cc9ad301db62b066d2cd5dcea76b07
Howard Kennedy <hkennedyvi@gmail.com>
2020-11-06 17:34:09 -0800

    proxy/heroku test

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/api',
+    '/api/menus',
     createProxyMiddleware({
       target: 'http://localhost:3002',
       changeOrigin: true,
